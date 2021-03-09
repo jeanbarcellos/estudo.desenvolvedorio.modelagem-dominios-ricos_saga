0b688c196308b5402a0087f006c692b44c90024e
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-03-08 22:31:43 -0300

    Criação do MVC

---
+++ RebusNetCore/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Pedido.Commands;
+using Rebus.Bus;
+using RebusNetCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RebusNetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IBus _bus;
+
+        public HomeController(IBus bus)
+        {
+            _bus = bus;
+        }
+
+        public IActionResult Index()
+        {
+            _bus.Send(new RealizarPedidoCommand { AggregateRoot = Guid.NewGuid() }).Wait();
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ RebusNetCore/Startup.cs
@@ -0,0 +1,99 @@
+using Core.Messages;
+using Core.Messages.IntegrationEvents;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Pagamento;
+using Pagamento.Commands;
+using Pedido;
+using Pedido.Commands;
+using Rebus.Config;
+using Rebus.Persistence.InMem;
+using Rebus.Routing.TypeBased;
+using Rebus.ServiceProvider;
+using Rebus.Transport.InMem;
+
+
+namespace RebusNetCore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Configure and register Rebus
+
+            var nomeFila = "fila_rebus";
+
+            services.AddRebus(configure => configure
+                .Transport(t => t.UseInMemoryTransport(new InMemNetwork(), nomeFila))
+                //.Transport(t => t.UseRabbitMq("amqp://localhost", nomeFila))
+                .Subscriptions(s => s.StoreInMemory())
+                .Routing(r =>
+                {
+                    r.TypeBased()
+                        .MapAssemblyOf<Message>(nomeFila)
+                        .MapAssemblyOf<RealizarPedidoCommand>(nomeFila)
+                        .MapAssemblyOf<RealizarPagamentoCommand>(nomeFila);
+                })
+                .Sagas(s => s.StoreInMemory())
+                .Options(o =>
+                {
+                    o.SetNumberOfWorkers(1);
+                    o.SetMaxParallelism(1);
+                    o.SetBusName("Demo Rebus");
+                })
+            );
+
+            // Register handlers
+            services.AutoRegisterHandlersFromAssemblyOf<PagamentoCommandHandler>();
+            services.AutoRegisterHandlersFromAssemblyOf<PedidoSaga>();
+
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseRebus(c =>
+            {
+                c.Subscribe<PedidoRealizadoEvent>().Wait();
+                c.Subscribe<PagamentoRealizadoEvent>().Wait();
+                c.Subscribe<PedidoFinalizadoEvent>().Wait();
+                c.Subscribe<PagamentoRecusadoEvent>().Wait();
+                c.Subscribe<PedidoCanceladoEvent>().Wait();
+            });
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
